54d81f3fc130171f44e942bc5568351a77dd647d
zhaoshengjun <zhao.shengjun@gmail.com>
2018-05-14 23:10:49 +1000

    use enzyme-to-json to format the output

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { configure, shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import toJson from "enzyme-to-json";
 
 configure({ adapter: new Adapter() });
 
@@ -14,6 +15,6 @@ describe("<App />", () => {
 
   it("matches the snapshot", () => {
     const tree = shallow(<App />);
-    expect(tree).toMatchSnapshot();
+    expect(toJson(tree)).toMatchSnapshot();
   });
 });
