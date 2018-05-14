c20c10c48aa0b873cc59713e267f480fe99c1e76
zhaoshengjun <zhao.shengjun@gmail.com>
2018-05-14 23:01:19 +1000

    back to original version

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-const Test = () => <div>Test</div>;
 class App extends Component {
   render() {
     return (
@@ -11,15 +10,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <ul className="tylor">
-          <li>Test1</li>
-          <li>Test2</li>
-          <li>Test3</li>
-        </ul>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Test />
       </div>
     );
   }
