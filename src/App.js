6872bb1979fcb13cb7593b40364fe079664ba96a
zhaoshengjun <zhao.shengjun@gmail.com>
2018-05-14 22:41:32 +1000

    shallow won't  render child component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
+const Test = () => <div>Test</div>;
 class App extends Component {
   render() {
     return (
@@ -13,6 +14,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Test />
       </div>
     );
   }
