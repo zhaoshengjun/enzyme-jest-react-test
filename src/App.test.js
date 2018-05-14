8343ffd23fbb2928a7ef0599b3cc457d5cbba386
zhaoshengjun <zhao.shengjun@gmail.com>
2018-05-14 22:43:05 +1000

    addtional parameters for shallow function

--- src/App.test.js
+++ src/App.test.js
@@ -8,7 +8,10 @@ configure({ adapter: new Adapter() });
 
 describe("<App />", () => {
   it("should render App", () => {
-    const wrapper = shallow(<App />);
+    const wrapper = shallow(<App />, {
+      context: {},
+      disableLifecycleMethods: false
+    });
     console.log(wrapper.debug());
   });
 });
